3d8d3bd62bcc83133b4f35ea7f838a6b91d1bc5c
Justin Perez <justinmp215@gmail.com>
2023-09-14 00:06:25 -0700

    fix(zellij): better settings

--- home-manager/justin/features/cli/zellij.nix
+++ home-manager/justin/features/cli/zellij.nix
@@ -4,6 +4,11 @@
     enableFishIntegration = true;
 
     settings = {
+      # default_layout = "compact";
+      mouse_mode = true;
+      copy_on_select = true;
+      simplified_ui = true;
+      on_force_close = "detach";
       keybinds = {
         unbind = [ "Ctrl s" "Ctrl q" ];
       };
