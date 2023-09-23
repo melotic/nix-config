afe1a6cb017da5e8ee2ff594fec4ccf0a09c1d79
Justin Perez <justinmp215@gmail.com>
2023-09-23 13:20:25 -0700

    feat(alacritty): add `nix-colors`

--- home-manager/justin/features/desktop/alacritty.nix
+++ home-manager/justin/features/desktop/alacritty.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.alacritty = {
     enable = true;
 
@@ -11,6 +11,26 @@
         normal.family = "DroidSansM Nerd Font";
         bold.family = "DroidSansM Nerd Font";
         italic.family = "DroidSansM Nerd Font";
+      };
+
+      # use nix-colors
+      colors = {
+        primary = {
+          foreground = "#${config.colorScheme.colors.base05}";
+          background = "#${config.colorScheme.colors.base00}";
+        };
+
+        # normal colors
+        normal = {
+          black = "#${config.colorScheme.colors.base00}";
+          red = "#${config.colorScheme.colors.base08}";
+          green = "#${config.colorScheme.colors.base0B}";
+          yellow = "#${config.colorScheme.colors.base0A}";
+          blue = "#${config.colorScheme.colors.base0D}";
+          magenta = "#${config.colorScheme.colors.base0E}";
+          cyan = "#${config.colorScheme.colors.base0C}";
+          white = "#${config.colorScheme.colors.base05}";
+        };
       };
     };
   };
