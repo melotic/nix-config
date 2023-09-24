fa5d9ea4ddcbe1e2d5cfec0093c5267b307f7924
Justin Perez <justinmp215@gmail.com>
2023-09-23 22:56:57 -0700

    feat(podman): add `docker` compat

--- nixos/common/optional/podman.nix
+++ nixos/common/optional/podman.nix
@@ -1,5 +1,9 @@
 {
   virtualisation.podman = {
     enable = true;
+    dockerCompat = true;
+
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
   };
 }
