a932db5cf5035b9318c5360596051f9f2bf984a8
Justin Perez <justinmp215@gmail.com>
2023-09-23 22:56:21 -0700

    fix(edge): remove `flags.conf`

--- home-manager/justin/features/desktop/edge.nix
+++ home-manager/justin/features/desktop/edge.nix
@@ -2,10 +2,4 @@
   home.packages = with pkgs; [
     microsoft-edge-dev
   ];
-
-  # use wayland
-  home.file.".config/edge-dev-flags.conf".text = ''
-    --enable-features=UseOzonePlatform
-    --ozone-platform=wayland
-  '';
 }
