24847a0861450d580c6ea21af17a5372836714ac
Justin Perez <justinmp215@gmail.com>
2023-09-23 22:56:02 -0700

    feat(desktop): add jetbrains ides & discord

--- home-manager/justin/features/desktop/default.nix
+++ home-manager/justin/features/desktop/default.nix
@@ -12,5 +12,14 @@
   home.packages = with pkgs; [
     bitwarden
     slack
+    jetbrains.rider
+    jetbrains.rust-rover
+    discord
   ];
+
+
+  # set environment variables to fix issues on Wayland
+  home.sessionVariables = {
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+  };
 }
