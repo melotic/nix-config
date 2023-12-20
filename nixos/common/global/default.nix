4cd54c48bd48da223eba95b6f13bc5dc6cb05c75
Justin Perez <justinmp215@gmail.com>
2023-12-20 09:12:30 -0600

    feat: use cloudflare DNS

--- nixos/common/global/default.nix
+++ nixos/common/global/default.nix
@@ -16,7 +16,10 @@
 
   hardware.enableRedistributableFirmware = true;
 
-  networking.networkmanager.enable = true;
+  networking = {
+    networkmanager.enable = true;
+    nameservers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+  };
   systemd.services.NetworkManager-wait-online.enable = false;
 
   environment.enableAllTerminfo = true;
